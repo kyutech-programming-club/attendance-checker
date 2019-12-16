8fbbfee7522d69c2e99274839f06cde7732a21d5
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-12-16 15:47:00 +0900

    Save new_user to DB

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from flask import request, redirect, url_for, render_template, flash
-from main import app
+from main import app, db
+from main.models import User
+
+def save_user(user_name):
+    user = User(name = user_name)
+    db.session.add(user)
+    db.session.commit()
+    print("save new user:", user_name)
 
 @app.route('/')
 def index():
@@ -8,9 +15,11 @@ def index():
 @app.route('/users/create', methods=['GET', 'POST'])
 def new_user():
     if request.method == 'POST':
+        user_name = request.form['user_name']
 
-        if request.form['user_name'] == "" :
+        if user_name == "" :
             return render_template('new_user.html')
         else :
+            save_user(user_name)
             return redirect(url_for('index'))
     return render_template('new_user.html')
