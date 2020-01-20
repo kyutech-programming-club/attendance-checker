fa0fb757617f3332231aed48980a6d3a7ab317b9
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-01-20 15:23:10 +0900

    N-to-N ralation User and Date class

--- main/models.py
+++ main/models.py
@@ -1,30 +1,50 @@
 from main import db
 
+user_date_table = db.Table('user_date_tables',
+                            db.Column('user_id', 
+                                      db.Integer,
+                                      db.ForeignKey('users.user_id'),
+                                      primary_key=True
+                                      ),
+                            db.Column('date_id',
+                                      db.Integer,
+                                      db.ForeignKey('dates.date_id'),
+                                      primary_key=True
+                                      )
+                            )
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, default = 'proken-taro', nullable = False)
     active = db.Column(db.Boolean, nullable=False, default=False)
-    dates = db.relationship("Date", lazy="select", backref=db.backref("user", lazy='joined'))
+    dates = db.relationship('Date',
+                            secondary=user_date_table, 
+                            lazy='subquery', 
+                            backref=db.backref('subscribers', lazy='dynamic')
+                            )
+
     @classmethod
     def authenticate(cls, query, name):
         user = query(cls).filter(cls.name==name).first()
         return user
 
     def __repr__(self):
-        return '<User id={self.id} name={self.name!r} active={self.active}>'.format(self=self)
+        return '<User id={self.user_id} name={self.name!r} active={self.active}>'.format(self=self)
 
 class Date(db.Model):
     __tablename__ = "dates"
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    start = db.Column(db.DateTime, nullable=True)
-    end = db.Column(db.DateTime, nullable=True)
+    date_id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.DateTime, nullable=True)
+    users = db.relationship('User', 
+                            secondary=user_date_table,
+                            lazy='subquery',
+                            backref=db.backref('subscribers', lazy='dynamic')
+                            )
 
     def __repr__(self):
-        return '<Id={self.id} User id={self.user_id} start={self.start} end={self.end}>'.format(self=self)
+        return '<Id={self.date_id} date={self.day}>'.format(self=self)
 
 class Proken(db.Model):
     __tablename__ = "prokens"
