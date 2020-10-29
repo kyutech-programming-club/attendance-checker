ed25975d161ded4ab45843edf54b30361b92392a
syu-kwsk <wataru1165kwsk@gmail.com>
2020-10-29 13:52:53 +0900

    Add app-bar

--- vue-webapp/frontend/src/App.vue
+++ vue-webapp/frontend/src/App.vue
@@ -1,33 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <v-app>
+    <v-app-bar
+      dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/attend">Attend</router-link>
-    </div>
-    <router-view/>
-  </div>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
 </style>
